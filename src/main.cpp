a13f97cdf6304307c6c3f509ac9e951a14b37ab5
JP Sheehan <jp.sheehan2@gmail.com>
2025-07-24 20:06:01 -0400

    Access chained shift registers

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,15 @@
 #include <Arduino.h>
 
 // Define constants for pin modes
-const uint8_t DATA_PIN = 2;  // Example pin for data input
-const uint8_t CLK1_PIN = 4;  // Example pin for clock input
-const uint8_t SHLD1_PIN = 3; // Example pin for shift input
-const uint8_t CLK2_PIN = 6;  // Example pin for second clock input
-const uint8_t SHLD2_PIN = 5; // Example pin for second shift input
+const uint8_t DATA_PIN = 2; // Example pin for data input
+const uint8_t CLK_PIN = 4;  // Example pin for clock input
+const uint8_t SHLD_PIN = 3; // Example pin for shift input
 
-// define button_signal funciton
-void button_signal() {
-  if (digitalRead(DATA_PIN) == HIGH) {
-    digitalWrite(LED_BUILTIN, HIGH);
-  } else {
-    digitalWrite(LED_BUILTIN, LOW);
-  }
-}
-
-void cycle_mux(uint8_t mux) {
-  if (mux == 1) {
-    digitalWrite(CLK1_PIN, HIGH); // Set clock pin high
-    delay(1);                     // Small delay for stability
-    digitalWrite(CLK1_PIN, LOW);  // Set shift pin low
-  } else if (mux == 2) {
-    digitalWrite(CLK2_PIN, HIGH); // Set clock pin high
-    delay(1);                     // Small delay for stability
-    digitalWrite(CLK2_PIN, LOW);  // Set shift pin low
-  }
-  delay(1); // Small delay for stability
+void cycle_mux() {
+  digitalWrite(CLK_PIN, HIGH); // Set clock pin high
+  delay(1);                    // Small delay for stability
+  digitalWrite(CLK_PIN, LOW);  // Set shift pin low
+  delay(1);                    // Small delay for stability
 }
 
 void setup() {
@@ -34,22 +17,28 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   // initialize pin 2 as input with pull-down resistor
   pinMode(DATA_PIN, INPUT_PULLDOWN);
-  digitalWrite(CLK1_PIN, LOW);  // Set clock pin low
-  digitalWrite(SHLD1_PIN, LOW); // Set shift pin low
-  delay(1);                     // Small delay for stability
+  digitalWrite(CLK_PIN, LOW);  // Set clock pin low
+  digitalWrite(SHLD_PIN, LOW); // Set shift pin low
+  delay(1);                    // Small delay for stability
 }
 
 void loop() {
-  digitalWrite(CLK1_PIN, LOW);   // Set clock pin low
-  digitalWrite(SHLD1_PIN, HIGH); // Set shift pin low
-  delay(1);                      // Small delay for stability
+  digitalWrite(CLK_PIN, LOW);   // Set clock pin low
+  digitalWrite(SHLD_PIN, HIGH); // Set shift pin low
+  delay(1);                     // Small delay for stability
 
-  cycle_mux(1);    // Cycle through the first multiplexer
-  cycle_mux(1);    // Cycle through the first multiplexer
-  button_signal(); // Call the button signal function
+  int n = 15; // Number of cycles to perform
+  for (int i = 0; i < n; i++) {
+    cycle_mux(); // Cycle through the first multiplexer
+  }
 
-  digitalWrite(CLK1_PIN, LOW);  // Set clock pin low
-  digitalWrite(SHLD1_PIN, LOW); // Set shift pin low
+  if (digitalRead(DATA_PIN) == HIGH) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
 
-  delay(10); // small delay to debounce
+  digitalWrite(CLK_PIN, LOW);  // Set clock pin low
+  digitalWrite(SHLD_PIN, LOW); // Set shift pin low
+  delay(10);                   // small delay to debounce
 }
