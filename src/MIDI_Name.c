bd05c1cc7d873c50b350c66c3251838d5e597361
JP Sheehan <jp.sheehan2@gmail.com>
2025-07-25 08:41:17 -0400

    Change board name

---
+++ src/MIDI_Name.c
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+const uint8_t usb_string_product_name[] = {
+  24,  // length (2 bytes per char + 2)
+  3,   // descriptor type
+  'O', 0, 'r', 0, 'g', 0, 'a', 0, 'n', 0, ' ', 0, 'B', 0, 'o', 0, 'a', 0, 'r', 0, 'd', 0
+};
+#ifdef __cplusplus
+}
+#endif
