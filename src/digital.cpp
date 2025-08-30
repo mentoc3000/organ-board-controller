538b74dcdb06f307542b3cc697186829ff592e16
JP Sheehan <jp.sheehan2@gmail.com>
2025-08-30 11:17:23 -0400

    Accomodate many buttons

--- src/digital.cpp
+++ src/digital.cpp
@@ -5,6 +5,8 @@
 const uint8_t DATA_PIN = 2;
 const uint8_t CLK_PIN = 4;
 const uint8_t SHLD_PIN = 3;
+const uint8_t DIGITAL_CHANNEL = 1;
+const uint8_t NUM_BUTTONS = 1; // Update this if more buttons are added
 
 void cycle_mux() {
   digitalWrite(CLK_PIN, HIGH);
@@ -13,43 +15,51 @@ void cycle_mux() {
   delay(1);
 }
 
+// Button class
+class Button {
+public:
+  Button(uint8_t cc) : cc(cc), lastState(LOW) {}
+
+  void update(bool currentState) {
+    if (currentState == HIGH && lastState == LOW) {
+      // Button was just toggled on
+      usbMIDI.sendControlChange(cc, 127, DIGITAL_CHANNEL);
+      updateDisplay(cc, 127);
+    } else if (currentState == LOW && lastState == HIGH) {
+      // Button was just toggled off
+      usbMIDI.sendControlChange(cc, 0, DIGITAL_CHANNEL);
+      updateDisplay(cc, 0);
+    }
+    lastState = currentState;
+  }
+
+private:
+  uint8_t cc;
+  bool lastState;
+};
+
+Button buttons[NUM_BUTTONS];
+
 void setupDigitalIO() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(DATA_PIN, INPUT_PULLDOWN);
   digitalWrite(CLK_PIN, LOW);
   digitalWrite(SHLD_PIN, LOW);
+  for (uint8_t i = 0; i < NUM_BUTTONS; i++) {
+    buttons[i] = Button(1 + i); // Assign CC numbers starting from 1
+  }
 }
-
-// State variable for button edge detection
-bool lastButtonState = LOW;
 
 void loopDigitalIO() {
   digitalWrite(CLK_PIN, LOW);
   digitalWrite(SHLD_PIN, HIGH);
   delay(1);
 
-  int n = 15; // Number of cycles to perform
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < NUM_BUTTONS; i++) {
     cycle_mux();
+    bool buttonState = digitalRead(DATA_PIN);
+    buttons[i].update(buttonState);
   }
-
-  bool buttonState = digitalRead(DATA_PIN);
-  if (buttonState == HIGH) {
-    // digitalWrite(LED_BUILTIN, HIGH);
-    if (lastButtonState == LOW) {
-      // Button was just pressed, send MIDI CC
-      usbMIDI.sendControlChange(1, 127, 1); // CC#1, value 127, channel 1
-      updateDisplay(1, 127);
-    }
-  } else {
-    // digitalWrite(LED_BUILTIN, LOW);
-    if (lastButtonState == HIGH) {
-      // Button was just released, send MIDI CC off (optional)
-      usbMIDI.sendControlChange(1, 0, 1); // CC#1, value 0, channel 1
-      updateDisplay(1, 0);
-    }
-  }
-  lastButtonState = buttonState;
 
   digitalWrite(CLK_PIN, LOW);
   digitalWrite(SHLD_PIN, LOW);
